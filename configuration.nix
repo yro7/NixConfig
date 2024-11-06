ff5aa10b909acbc6d29b7f1dd64ba816b06816da
Made <52044171+yro7@users.noreply.github.com>
2024-11-06 18:46:21 +0100

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,5 +1,6 @@
-# /etc/nixos/configuration.nix
-# Main configuration.nix file
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, ... }:
 
@@ -9,7 +10,7 @@
       ./hardware-configuration.nix
 	<home-manager/nixos>
 	./home-manager/home-manager.nix
-#	./openvpn/openvpn.nix
+#	./openvpn.nix WIP 
     ];
 
   # Close lid behaviour
@@ -52,6 +53,10 @@
     LC_TIME = "en_US.UTF-8";
   };
 
+
+  # Configure keymap in X11
+  console.keyMap = "fr";
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.nix = {
     isNormalUser = true;
@@ -86,8 +91,40 @@
 	tmate
 	upower
 	speedtest-cli
-	easyrsa
-	bat
   ];
-	system.stateVersion = "24.05";
+
+
+  services.jellyfin = {
+	enable = true;
+	openFirewall = true;
+  };
+
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  networking.firewall.allowedTCPPorts = [ 8096 ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+
 }
